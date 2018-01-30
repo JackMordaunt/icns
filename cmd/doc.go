5fdb3ed2de4345cf12601fc28b0f5badff095f80
Jack Mordaunt <jackmordaunt@gmail.com>
2018-01-30 17:30:31 +0100

    [+] More thorough usage message.

---
+++ cmd/doc.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+)
+
+func usage() {
+	fmt.Printf("\n")
+	pflag.Usage()
+	fmt.Printf(`
+You can also pipe to stdin and from stdout. 
+The pipes will be detected automatically, and both --input and --output will be ignored.
+
+		cat icon.png | icns | cat > icon.icns
+
+`)
+}
