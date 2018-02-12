540bed170fabdf636b6b0b3cb7b74b02276c286c
Jack Mordaunt <jackmordaunt@gmail.com>
2018-02-12 14:15:35 +0100

    [+] Documened the purpose of the lib and the included CLI app.

--- doc.go
+++ doc.go
@@ -6,6 +6,15 @@
 // a Mac native cli utility, or 2. use tools that wrap `ImageMagick` which adds
 // a large dependency to your project for such a simple use case.
 //
+// With this library you can use pure Go to create icns files from any source
+// image, given that you can decode it into an `image.Image`, without any
+// heavyweight dependencies or subprocessing required. You can also use this
+// library to create icns files on windows and linux.
+//
+// A small CLI app `icnsify` is provided to allow you to create icns files
+// using this library from the command line. It supports piping, which is
+// something `iconutil` does not do, making it substantially easier to wrap.
+//
 // Note: All icons within the icns are sized for high dpi retina screens, using
 // the appropriate icns OSTypes.
 package icns
