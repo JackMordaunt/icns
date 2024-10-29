887a11b796668020f14e22191ded6be8767d2639
Jack Mordaunt <jackmordaunt.dev@gmail.com>
2024-10-29 10:13:19 +0800

    icns: delete panicf (unused)
    Signed-off-by: Jack Mordaunt <jackmordaunt.dev@gmail.com>

--- error.go
+++ error.go
@@ -16,7 +16,3 @@ func (err ErrImageTooSmall) Error() string {
 	format := "image is too small: %dx%d, need at least %dx%d"
 	return fmt.Sprintf(format, b.X, b.Y, err.need, err.need)
 }
-
-func panicf(format string, values ...interface{}) {
-	panic(fmt.Sprintf(format, values...))
-}
